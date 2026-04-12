eb11711a78f2fb43c248685d0e1dd759b366166c
anu-9674 <anu.ghosh9674@gmail.com>
2026-04-12 16:05:20 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import subprocess
-scripts = ["graph_generator.py", "in_context_learning_examples.py", "Query_input_builder.py"]
+scripts = ["graph_generator.py", "in_context_learning_examples.py", "Query_input_builder.py","LLM_Manegement.py"]
 for script in scripts:
     subprocess.run(["python", script])
